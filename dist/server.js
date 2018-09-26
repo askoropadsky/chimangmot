2d496cd920b6fa1e97599181ecbefc7b77b41dd9
analytics.generator <analytics.generator@at.kinsa.com>
2018-09-26 13:19:05 -0700

    change port 3000 -> 8080

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app_1.default.listen(port, (err) => {
     if (err) {
         return console.log(err);
